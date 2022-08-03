2f9a4fc6400e8a5123c0ce5435985f1a6278c0ca
HMyers9898 <102501642+HMyers9898@users.noreply.github.com>
2022-08-02 21:57:03 -0600

    first push

---
+++ VariablesAndOperations.js
@@ -0,0 +1,41 @@
+//create a variable to hold the quantity of available plane seats left on a flight
+var seatsOpen=68;
+		
+//create a variable to hold the cost of groceries at checkout
+var costOfGroceries="$76"
+
+//create a variable to hold a person's middle initial
+var middleInitial="G";
+
+//create a variable to hold true if it's hot outside and false if it's cold outside
+let isHotOutside=true;
+
+//create a variable to hold a customer's first name
+var firstName="Hannah";
+
+//create a variable to hold a street address
+var streetAddress="6213 Barnhart St Ne"
+
+//print all variables to the console
+console.log(middleInitial+firstName+streetAddress);
+
+
+
+//a customer booked 2 plane seats, remove 2 seats from the available seats variable
+var seatsOpen = seatsOpen - 2;
+
+//impulse candy bar purchase, add 2.15 to the grocery total
+var costOfGroceries = costOfGroceries + 2.15;
+
+
+//birth certificate was printed incorrectly, change the middle initial to something else
+var middleInitial="H";
+
+//the season has changed, update the hot outside variable to be opposite of what it was
+isHotOutside=!isHotOutside;
+
+//create a new variable called full name using the customer's first name, the middle initial, and a last name of your choice
+var fullName= firstName + middleInitial + "Myers";
+
+//print a line to the console that introduces the customer and says they live at the address variable
+console.log(fullName);
